ec44f207299d9a1a2942ce48ef7092893f1f0044
Karmmah <paddi96@web.de>
2022-10-11 23:01:19 +0200

    interface update

--- weather_gui.py
+++ weather_gui.py
@@ -18,19 +18,20 @@ def get_image(epd_width, epd_height, town, current, forecast): #return an image 
 	draw = ImageDraw.Draw(image)
 
 	#general info (town and time)
-	w,h = draw.textsize(town)
-	draw.text((epd_height*0.3, -h*0.3), text=town)
+#	w,h = draw.textsize(town)
+	draw.text((50, 11), text=town)
 	_time_ = time.strftime('%H:%M:%S')
 	w,h = draw.textsize(_time_)
-	draw.text((epd_height*0.3, h*0.55), text=_time_)
+	draw.text((50, 19), text=_time_)
 
 	#main weather info
 	temp = current[0]
 	w,h = draw.textsize(temp,font=large_font)
-	draw.text((46-w,-9),text=temp,font=large_font,outline=0)
-	draw.text((47,-4),text='*C',font=text_font)
+	draw.text( (46-w,-10), text=temp, font=large_font, outline=0 )
+#	draw.text((47,-4),text='*C',font=text_font)
+	draw.text( (47,0), text='*C')
 	condition = current[1]
-	draw.text((47,12),text=condition,font=text_font)
+	draw.text( (69,-7), text=condition, font=text_font )
 
 	#wind gauge
 	radius = 25 #pixels
@@ -65,13 +66,14 @@ def draw_today(draw, forecast, min_max, epd_width, epd_height):
 	draw.rectangle([(epd_height-width,epd_width-1),(epd_height-1,epd_width-height)]) #draw border
 
 	draw.text((epd_height-width+3, epd_width-height), text="Today")
+	labels = ["T", "W", "C", "R"]
 	for i in range(amount):
 		draw.line([ epd_height-width, epd_width-line_height*(amount-i), epd_height, epd_width-line_height*(amount-i) ]) #divider line
 
-		draw.text( (epd_height-30,epd_width-line_height*(amount+i)) ,text="field"+str(i), font=text_font, fill=0, align='center')
+		draw.text( (epd_height-46,epd_width-line_height*(amount-i)) ,text=labels[i], font=small_font, fill=0, align='center')
 		for j in range(1, len(forecast[2])):
-			y0 = epd_width-line_height*i - line_height * (forecast[j-1][i+2]-min_max[i][0]) / (min_max[i][1]-min_max[i][0])
-			y1 = epd_width-line_height*i - line_height * (forecast[j][i+2]-min_max[i][0])/(min_max[i][1]-min_max[i][0])
+			y0 = epd_width-line_height*(amount-i-1) - line_height * (forecast[j-1][i+2]-min_max[i][0]) / (min_max[i][1]-min_max[i][0])
+			y1 = epd_width-line_height*(amount-i-1) - line_height * (forecast[j][i+2]-min_max[i][0])/(min_max[i][1]-min_max[i][0])
 			x0 = epd_height-width+j*width/len(forecast[2])
 			x1 = x0 + width/len(forecast[2])
 			draw.line([x0,y0,x1,y1])
@@ -102,25 +104,28 @@ def draw_graphical_forecast(epd_width, epd_height, draw, forecast, min_max, star
 
 		for j in range(0,len(forecast)): #draw entries
 			if i == 0: #draw vertical separators on first line
-				if forecast[j][0][11:13] == "00": #vertical lines; print a wider line at the start of each day
-					draw.line([(j*width,epd_width),(j*width,epd_width-height*amount)], width=2)
-				else:
-					draw.line([(j*width,epd_width),(j*width,epd_width-height*amount)], width=1)
+				if forecast[j][0][11:13] == "00": #vertical lines; at the start of each day
+#					draw.line([(j*width,epd_width),(j*width,epd_width-height*amount)], width=2)
+					draw.line([(j*width,epd_width),(j*width,epd_width-height*amount)])
+				else: #at each other data point during the day
+					pass #draw.line([(j*width,epd_width),(j*width,epd_width-height*amount)], width=1)
 			value = (float(forecast[j][i+2])-min_max[i][0])/(min_max[i][1]-min_max[i][0]+0.001)
 			y_right = y0-height*value
-			draw.line([((j-1)*width,y_left), (j*width,y_right)], width=3) #graph line
+			draw.line([((j-1)*width,y_left), (j*width,y_right)], width=1) #graph line
 			y_left = y_right
 
-		draw.text((epd_height-xborder_right-23,y0-height*1.1),text=str(min_max[i][1])) #min and max values of each line
-		draw.text((epd_height-xborder_right-23,y0-height*0.58),text=str(min_max[i][0]))
+#		draw.text((epd_height-xborder_right-23,y0-height*1.1),text=str(min_max[i][1]), font=small_font) #min and max values of each line
+#		draw.text((epd_height-xborder_right-23,y0-height*0.58),text=str(min_max[i][0]), font=small_font)
+		draw.text((epd_height-xborder_right-21,y0-height*1.13),text=str(min_max[i][1]), font=small_font) #min and max values of each line
+		draw.text((epd_height-xborder_right-21,y0-height*0.64),text=str(min_max[i][0]), font=small_font)
 
 	w,h = draw.textsize('W')
-	draw.text((epd_height-xborder_right+2,epd_width-height*5.5-h/2),text='T') #labels
-	draw.text((epd_height-xborder_right+2,epd_width-height*4.5-h/2),text='W')
-	draw.text((epd_height-xborder_right+2,epd_width-height*3.5-h/2),text='C')
-	draw.text((epd_height-xborder_right+2,epd_width-height*2.5-h/2),text='R')
-	draw.text((epd_height-xborder_right+2,epd_width-height*1.5-h/2),text='P')
-	draw.text((epd_height-xborder_right+2,epd_width-height*0.5-h/2),text='H')
+	draw.text((epd_height-xborder_right+2,epd_width-height*5.5-h/2),text='T', font=small_font) #labels
+	draw.text((epd_height-xborder_right+2,epd_width-height*4.5-h/2),text='W', font=small_font)
+	draw.text((epd_height-xborder_right+2,epd_width-height*3.5-h/2),text='C', font=small_font)
+	draw.text((epd_height-xborder_right+2,epd_width-height*2.5-h/2),text='R', font=small_font)
+	draw.text((epd_height-xborder_right+2,epd_width-height*1.5-h/2),text='P', font=small_font)
+	draw.text((epd_height-xborder_right+2,epd_width-height*0.5-h/2),text='H', font=small_font)
 
 	w = draw.textsize(end)[0]
 	draw.text((0, epd_width-height*amount-h), text=start)
