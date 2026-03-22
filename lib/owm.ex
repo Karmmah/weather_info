cd69f944cedc90651a926c52d486289f9379a32f
pi <pi@alpine-headless.fritz.box>
2026-03-22 11:12:34 +0100

    fixed rain prob drawing

--- lib/owm.ex
+++ lib/owm.ex
@@ -97,7 +97,7 @@ defmodule EXW.OWM do
                 temp: Float.round(dp["main"]["temp"] - 273.15, 1),
                 humidity: dp["main"]["humidity"],
                 cloud_cov: dp["clouds"]["all"],
-                rain_prob: dp["pop"],
+                rain_prob: dp["pop"]*100,
                 pressure: dp["main"]["pressure"],
                 wind_dir: dp["wind"]["deg"],
                 wind_spd: dp["wind"]["speed"],
